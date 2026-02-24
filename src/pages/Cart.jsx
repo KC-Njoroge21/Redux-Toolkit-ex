274410736d7b4f76f3099253e95e2a79ba409dfb
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-02-24 10:13:02 +0300

    Created and styled the placeOrder button

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -13,9 +13,15 @@ const Cart = () => {
   })
 
   return (
-    <div className='flex flex-col gap-3 p-3 items-center'>
-      <h2 className='text-2xl font-semibold'>Your Cart</h2>
-      {produts}
+    <div className=''>
+      <h2 className='text-2xl text-center font-semibold'>Your Cart</h2>
+      <div className='flex flex-col items-center gap-3 p-3'>
+        {produts}
+      </div>
+        <h2 className='text-center text-2xl font-semibold mb-4'>Total: 200</h2>
+        <div className=' flex justify-center '>
+        <button className='border cursor-pointer p-2 text-lg bg-sky-300 font-semibold text-white rounded-lg '>Place Order</button>
+      </div>
       </div>
   )
 }
